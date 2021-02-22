622da769591807a85a25700b1c487caaf5d4c8d6
Marvin Ritter <marvinritter@google.com>
2021-02-22 02:26:35 -0800

    Document DatasetBuilder protocol required by the library.
    PiperOrigin-RevId: 358775733

--- clu/deterministic_data.py
+++ clu/deterministic_data.py
@@ -61,6 +61,7 @@ import jax.numpy as jnp
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import typing_extensions
 
 
 
@@ -68,6 +69,16 @@ Tensor = Union[tf.Tensor, tf.SparseTensor, tf.RaggedTensor]
 Features = Dict[str, Tensor]
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+
+class DatasetBuilder(typing_extensions.Protocol):
+  """Protocol for dataset builders (subset of tfds.core.DatasetBuilder)."""
+
+  def as_dataset(
+      self, split: Union[str, tfds.core.ReadInstruction], shuffle_files: bool,
+      read_config: tfds.ReadConfig,
+      decoders: Optional[Dict[str, tfds.decode.Decoder]]) -> tf.data.Dataset:
+    ...
 
 
 def get_read_instruction_for_host(
@@ -213,7 +224,7 @@ def pad_dataset(dataset: tf.data.Dataset, *, batch_dims: Sequence[int],
   return dataset.concatenate(filler_dataset.repeat(padding))
 
 
-def create_dataset(dataset_builder,
+def create_dataset(dataset_builder: DatasetBuilder,
                    *,
                    split: Union[str, tfds.core.ReadInstruction],
                    batch_dims: Sequence[int] = (),
