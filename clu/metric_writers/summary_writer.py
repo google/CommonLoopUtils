2a0267a2fc6ce295ad98a4062c68fdc7c964dafa
Adam Roberts <adarob@google.com>
2022-01-06 11:53:47 -0800

    Call `SummaryWriter.set_as_default()` instead of using the context manager, avoiding the flush on context exit which sometimes results in a race condition.
    PiperOrigin-RevId: 420111333

--- clu/metric_writers/summary_writer.py
+++ clu/metric_writers/summary_writer.py
@@ -44,47 +44,47 @@ class SummaryWriter(interface.MetricWriter):
       self, step: int,
       values: Mapping[str, Array],
       metadata: Optional[Mapping[str, Any]] = None):
-    with self._summary_writer.as_default():
-      for key, value in values.items():
-        md = metadata.get(key) if metadata is not None else None
-        tf.summary.write(key, value, step=step, metadata=md)
+    self._summary_writer.set_as_default()
+    for key, value in values.items():
+      md = metadata.get(key) if metadata is not None else None
+      tf.summary.write(key, value, step=step, metadata=md)
 
   def write_scalars(self, step: int, scalars: Mapping[str, Scalar]):
-    with self._summary_writer.as_default():
-      for key, value in scalars.items():
-        tf.summary.scalar(key, value, step=step)
+    self._summary_writer.set_as_default()
+    for key, value in scalars.items():
+      tf.summary.scalar(key, value, step=step)
 
   def write_images(self, step: int, images: Mapping[str, Array]):
-    with self._summary_writer.as_default():
-      for key, value in images.items():
-        if len(value.shape) == 3:
-          value = value[None]
-        tf.summary.image(key, value, step=step, max_outputs=value.shape[0])
+    self._summary_writer.set_as_default()
+    for key, value in images.items():
+      if len(value.shape) == 3:
+        value = value[None]
+      tf.summary.image(key, value, step=step, max_outputs=value.shape[0])
 
   def write_audios(
       self, step: int, audios: Mapping[str, Array], *, sample_rate: int):
-    with self._summary_writer.as_default():
-      for key, value in audios.items():
-        tf.summary.audio(key, value, sample_rate=sample_rate, step=step,
-                         max_outputs=value.shape[0])
+    self._summary_writer.set_as_default()
+    for key, value in audios.items():
+      tf.summary.audio(key, value, sample_rate=sample_rate, step=step,
+                       max_outputs=value.shape[0])
 
   def write_texts(self, step: int, texts: Mapping[str, str]):
-    with self._summary_writer.as_default():
-      for key, value in texts.items():
-        tf.summary.text(key, value, step=step)
+    self._summary_writer.set_as_default()
+    for key, value in texts.items():
+      tf.summary.text(key, value, step=step)
 
   def write_histograms(self,
                        step: int,
                        arrays: Mapping[str, Array],
                        num_buckets: Optional[Mapping[str, int]] = None):
-    with self._summary_writer.as_default():
-      for key, value in arrays.items():
-        buckets = None if num_buckets is None else num_buckets.get(key)
-        tf.summary.histogram(key, value, step=step, buckets=buckets)
+    self._summary_writer.set_as_default()
+    for key, value in arrays.items():
+      buckets = None if num_buckets is None else num_buckets.get(key)
+      tf.summary.histogram(key, value, step=step, buckets=buckets)
 
   def write_hparams(self, hparams: Mapping[str, Any]):
-    with self._summary_writer.as_default():
-      hparams_api.hparams(dict(utils.flatten_dict(hparams)))
+    self._summary_writer.set_as_default()
+    hparams_api.hparams(dict(utils.flatten_dict(hparams)))
 
   def flush(self):
     self._summary_writer.flush()
